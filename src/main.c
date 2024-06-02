dbdde303e863a4f2d9c8c738df91844577a61ba3
Notforest <notforest69@gmail.com>
2024-06-02 22:59:13 +0200

    Added template files for initial setup.

---
+++ src/main.c
@@ -0,0 +1,15 @@
+/* 
+ *  Main executable code for 'Open Game Pad'
+ *
+ *  This code defines all necessary functions and stuctures for operating data between input sensors and output USB connector.
+ *  The code is interrupt driven, therefore all necessary initialization part can be found in main function, while the rest is
+ *  located in ISR definitions.
+ * */
+
+#include<avr/io.h>
+#include "ogpad.h"
+
+// Main function that initializes registers with required values and then waits for interrupts.
+int main(void) {
+    // TODO!
+}
---
+++ src/ogpad.h
@@ -0,0 +1,7 @@
+/* 
+ *  Header file that defines all required constants for 'Open Game Padu' main code.
+ * */
+
+#pragma once
+
+//TODO!
